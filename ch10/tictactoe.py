21182790da78d273a346506a19d87662110b5931
Anthony LoGreco <aclogreco@gmail.com>
2014-03-14 14:10:51 -0400

    Add inputPlayerLetter method to tictactoe.py module.

--- ch10/tictactoe.py
+++ ch10/tictactoe.py
@@ -19,3 +19,22 @@ def drawBoard(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
+
+def inputPlayerLetter():
+    """
+    Allows the player to choose what letter they want to be.
+    Returns a list with the player's letter as the first item, and the
+    computer's letter as the second.
+    """
+    letter = ''
+    while not (letter == 'X' or letter =='O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+
+    # The first element in the list is the player's letter, the second is the
+    # computer's letter.
+    if letter == 'X':
+        return ['X', 'O']
+    else
+        return ['O', 'X']
+
