d56420562804e59a91cb3787ee1b2bf9b10a6631
Anthony LoGreco <aclogreco@gmail.com>
2014-05-02 04:43:32 -0400

    Finish adding the rest of the functions needed for Tic-Tac-Toe.

--- ch10/tictactoe.py
+++ ch10/tictactoe.py
@@ -6,7 +6,6 @@ def drawBoard(board):
     """
     This function prints out the board that it was passed.
     """
-
     # "board" is a list of 10 strings representing the board (ignore index 0)
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
@@ -54,3 +53,118 @@ def playAgain():
     """
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
+
+def makeMove(board, letter, move):
+    """
+    Set the board at the move location to be the specified letter.
+    """
+    board[move] = letter
+
+def isWinner(bo, le):
+    """
+    Given a board and a player's letter, this function returns True if that
+    player has won.
+    bo = board and le = letter (abbreviations are used for brevity in the code)
+    """
+    return ((bo[7] == le and bo[8] == le and bo[9] = le) or #across the top
+            (bo[4] == le and bo[5] == le and bo[6] = le) or #across the middle
+            (bo[1] == le and bo[2] == le and bo[3] = le) or #across the bottom
+            (bo[7] == le and bo[4] == le and bo[1] = le) or #down the left side
+            (bo[8] == le and bo[5] == le and bo[2] = le) or #down the middle
+            (bo[9] == le and bo[6] == le and bo[3] = le) or #down the right side
+            (bo[7] == le and bo[5] == le and bo[3] = le) or #diagonal
+            (bo[9] == le and bo[5] == le and bo[1] = le))   #diagonal
+
+def getBoardCopy(board):
+    """
+    Make a duplicate of the board list and return the duplicate.
+    """
+    dupeBoard = []
+
+    for i in board:
+        dupeBoard.append(i)
+
+    return dupeBoard
+
+def isSpaceFree(board, move):
+    """
+    Return True if the passedmove is free on the passed board.
+    """
+    return board[move] == ' '
+
+def getPlayerMove(board):
+    """
+    Ask the player for their move.
+    """
+    move = ' '
+    
+    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+        print('What is your next move? (1-9)')
+        move = input()
+    
+    return int(move)
+
+def chooseRandomMoveFromList(board, movesList):
+    """
+    Returns a valid move from the passed list on the passed board.
+    Returns None if there is no valid move.
+    """
+    possibleMoves = []
+
+    for i in movesList:
+        if isSpaceFree(board, i):
+            possibleMoves.append(i)
+
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+        return None
+
+def getComputerMove(board, computerLetter):
+    """
+    Given a board amd the computer's letter, determine where to move and
+    return that move.
+    """
+    if computerLetter == 'X':
+        playerLetter = 'O'
+    else:
+        playerLetter = 'X'
+
+    # Algorithm for Tic-Tac-Toe AI:
+    # First, check if we can win in the next move
+    for i in range(1, 10):
+        copy = getBoardCopy(board)
+        if isSpaceFree(copy, i):
+            makeMove(copy, computerLetter, i)
+            if isWinner(copy, computerLetter):
+                return i
+
+    # Check if the player could win on their next move, and block them.
+    for i in range(1, 10):
+        copy = getBoardCopy(board)
+        if isSpaceFree(copy, i):
+            makeMove(copy, playerLetter, i)
+            if isWinner(copy, playerLetter):
+                return i
+
+    # Try to take one of the corners, if they are free.
+    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
+    if move != None:
+        return move
+
+    # Try to take the center, if it is free.
+    if isSpaceFree(board, 5):
+        return 5
+
+    # Move on one of the sides
+    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
+
+def isBoardFull(board):
+    """
+    Return True if every space on the board has been taken.
+    Otherwise return False.
+    """
+    for i in range(1, 10):
+        if isSpaceFree(board, i):
+            return False
+    return True
