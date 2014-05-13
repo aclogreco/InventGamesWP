3034abb1a85184a5f7e19053a9f464fe3c0d0fd5
Anthony LoGreco <aclogreco@gmail.com>
2014-05-13 05:09:13 -0400

    Add main game loop.

--- ch10/tictactoe.py
+++ ch10/tictactoe.py
@@ -34,7 +34,7 @@ def inputPlayerLetter():
     # computer's letter.
     if letter == 'X':
         return ['X', 'O']
-    else
+    else:
         return ['O', 'X']
 
 def whoGoesFirst():
@@ -168,3 +168,52 @@ def isBoardFull(board):
         if isSpaceFree(board, i):
             return False
     return True
+
+
+print("Welcome to Tic Tac Toe!")
+
+while True:
+    #Reset the board
+    theBoard = [' '] * 10
+    playerLetter, computerLetter = inputPlayerLetter()
+    turn = whoGoesFirst()
+    print('The ' + turn + ' will go first.')
+    gameIsPlaying = True
+
+    while gameIsPlaying:
+        if turn == "player":
+            # Player's turn.
+            drawBoard(theBoard)
+            move = getPlayerMove(theBoard)
+            makeMove(theBoard, playerLetter, move)
+
+            if isWinner(theBoard, playerLetter):
+                drawBoard(theBoard)
+                print('Hooray!!! You have won the game!')
+                gameIsPlaying = False
+            else
+                if isBoardFull(theBoard):
+                    drawBoard(theBoard)
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 'computer'
+        else:
+            # Computer's turn.
+            move = getComputerMove(theBoard, computerLetter)
+            makeMove(theBoard, computerLetter, move)
+
+            if isWinner(theBoard, computerLetter):
+                drawBoard(theBoard)
+                print('The computer has dominated you!!! You lose! Muhahahaha!!!')
+                gameIsPlaying = False
+            else:
+                if isBoardFull(theBoard):
+                    drawBoard(theBoard)
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 'player'
+
+    if not playAgain():
+        break
