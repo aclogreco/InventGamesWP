cab8a5b04349a38156c3bc558827cf6ce57fd0d3
Anthony LoGreco <aclogreco@gmail.com>
2014-11-06 21:33:25 -0500

    Finish the drawBoard function.

--- ch13/sonar.py
+++ ch13/sonar.py
@@ -10,4 +10,28 @@ def drawBoard(board):
     """
     Draw the board data structure.
     """
-    
+
+    hline = '     '  # initial space for the numbers down the left side of the board
+    for i in range(1, 6):
+        hline += (' ' * 9) + str(i)
+
+    # print the numbers across the top
+    print(hline)
+    print('   ' + ('0123456789' * 6))
+    print()
+
+    # print each of the 15 rows
+    for i in range(15):
+        # single-digit numbers need to be padded with an extra space
+        if i < 10:
+            extraSpace = ' '
+        else:
+            extraSpace = ''
+        print('%s%s %s %s' % (extraSpace, i, getRow(board, i), i))
+
+    # print the numbers across the bottom
+    print()
+    print('   ' + ('0123456789' * 6))
+    print(hline)
+
+
