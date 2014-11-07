0cdbbf36e453f25758ddd4e474af5aa47407b002
Anthony LoGreco <aclogreco@gmail.com>
2014-11-06 21:18:54 -0500

    Add the initial module file for the Sonar game in Chapter 13.

---
+++ ch13/sonar.py
@@ -0,0 +1,13 @@
+# sonar.py
+#
+# Sonar - an open seas treasure hunt game
+# A.C. LoGreco
+
+import random
+import sys
+
+def drawBoard(board):
+    """
+    Draw the board data structure.
+    """
+    
