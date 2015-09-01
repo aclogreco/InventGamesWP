ccf73b853425fd837efc816916c02e0caf3ff21f
Anthony LoGreco <aclogreco@gmail.com>
2015-09-01 03:41:52 -0400

    Add brute force decryption option to the caesar.py module.

--- ch14/caesar.py
+++ ch14/caesar.py
@@ -4,12 +4,12 @@ MAX_KEY_SIZE = 26
 
 def getMode():
     while True:
-        print('Do you wish to encrypt or decrypt a message?')
+        print('Do you wish to encrypt or decrypt or brute force a message?')
         mode = input().lower()
-        if mode in 'encrypt e decrypt d'.split():
+        if mode in 'encrypt e decrypt d brute b'.split():
             return mode
         else:
-            print('Enter either "encrypt" or "e" or "decrypt" or "d".')
+            print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
 
 
 def getMessage():
@@ -59,7 +59,12 @@ def getTranslatedMessage(mode, message, key):
 
 mode = getMode()
 message = getMessage()
-key = getKey()
+if mode[0] != 'b':
+    key = getKey()
 
 print('Your translated text is:')
-print(getTranslatedMessage(mode, message, key))
+if mode[0] != 'b':
+    print(getTranslatedMessage(mode, message, key))
+else:
+    for key in range(1, MAX_KEY_SIZE + 1):
+        print(key, getTranslatedMessage('decrypt', message, key))
