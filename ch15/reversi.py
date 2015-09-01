33a1af98999b200335a6800c3e2bb08574668a18
Anthony LoGreco <aclogreco@gmail.com>
2015-09-01 04:11:54 -0400

    Add drawBoard() function to the reversi.py module.

--- ch15/reversi.py
+++ ch15/reversi.py
@@ -1,3 +1,33 @@
 # reversi.py
 # Reversi (Othello)
+"""
+Reversi - a tile flipping game
+An example from Chapter 15 of 
+'Invent Your Own Games With Python' by Al Sweigart
+A.C. LoGreco
+"""
+
+import random
+import sys
+
+
+def drawBoard(board):
+    """
+    This function prints out the board that it was passed. Returns None.
+    """
+    HLINE = '  +---+---+---+---+---+---+---+---+'
+    VLINE = '  |   |   |   |   |   |   |   |   |'
+
+    print('    1   2   3   4   5   6   7   8')
+    print(HLINE)
+    for y in range(8):
+        print(VLINE)
+        print(y+1, end=' ')
+        for x in range(8):
+            print('| %s' % (board[x][y]), end=' ')
+        print('|')
+        print(VLINE)
+        print(HLINE)
+
+
 
