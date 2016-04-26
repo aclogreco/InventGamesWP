8a5be0bd8b000110ef62fac47b8b23ad5dbd589d
Anthony LoGreco <aclogreco@gmail.com>
2016-04-26 02:24:26 -0400

    Add isValidMove() function to reversi.py module.

--- ch15/reversi.py
+++ ch15/reversi.py
@@ -57,4 +57,60 @@ def getNewBoard():
     return board
 
 
+def isValidMove(board, tile, xstart, ystart):
+    """
+    Returns False if the player's move on space xstart, ystart is invalid.
+    If it is a valid move, returns a list of spaces that would become the
+    player's if they made a move here.
+    """
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+
+    board[xstart][ystart] = tile    # temporarily set the tile on the board.
+
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
+
+    tilesToFlip = []
+    for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1],
+                                   [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = xstart, ystart
+        x += xdirection     # first step in the direction
+        y += ydirection     # first step in the direction
+        if isOnBoard(x, y) and board[x][y] == otherTile:
+            # There is a piece belonging to the other player next to our piece.
+            x += xdirection
+            y += ydirection
+            if not isOnBoard(x, y):
+                continue
+            while board[x][y] == otherTile:
+                x += xdirection
+                y += ydirection
+                if not isOnBoard(x, y):
+                    # break out of while loop, then continue in for loop
+                    break
+            if not isOnBoard(x, y):
+                continue
+            if board[x][y] == tile:
+                # There are pieces to flip over. Go in the reverse direction
+                # until we reach the original space, noting all the tiles
+                # along the way.
+                while True:
+                    x -= xdirection
+                    y -= ydirection
+                    if x == xstart and y == ystart:
+                        break
+                    tilesToFlip.append([x, y])
+
+    board[xstart][ystart] = ' '     # restore the empty space
+
+    if len(tilesToFlip) == 0:
+        # If no tiles were flipped, this is not a valid move.
+        return False
+
+    return tilesToFlip
+
+
 
