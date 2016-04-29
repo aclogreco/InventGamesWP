a2586b1d7e9edb2935d7ce421106b1ca9440d1e8
Anthony LoGreco <aclogreco@gmail.com>
2016-04-29 19:09:27 -0400

    Add playAgain() function to the reversi.py module.

--- ch15/reversi.py
+++ ch15/reversi.py
@@ -196,4 +196,13 @@ def whoGoesFirst():
         return 'player'
 
 
+def playAgain():
+    """
+    This function returns True if the player wants to play again,
+    otherwise it returns False.
+    """
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
+
+
 
