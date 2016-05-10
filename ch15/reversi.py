f04ed6bafd7062e3c0b348c6a56f8b00b82d04e3
Anthony LoGreco <aclogreco@gmail.com>
2016-05-10 19:55:58 -0400

    Add getPlayerMove() function to the reversi.py module.

--- ch15/reversi.py
+++ ch15/reversi.py
@@ -246,5 +246,35 @@ def isOnCorner(x, y):
             (x == 7 and y == 7))
 
 
+def getPlayerMove(board, playerTile):
+    """
+    Let the player type in their move.
+    Returns the move as [x, y].
+    (or returns the strings 'hints' or 'quit')
+    """
+    DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
+    
+    while True:
+        print('Enter your move, or type quit to end the game, or hints to turn off/on hints.')
+        move = input().lower()
+        if move == 'quit':
+            return 'quit'
+        if move == 'hints':
+            return 'hints'
+
+        if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
+            x = int(move[0]) - 1
+            y = int(move[1]) - 1
+            if isValidMove(board, playerTile, x, y) == False:
+                continue
+            else:
+                break
+        else:
+            print('That is not a valid move. Type the x digit (1-8), then the y digit (1-8).')
+            print('For example, 81 will be the top-right corner.')
+
+    return [x, y]
+
+
 
 
