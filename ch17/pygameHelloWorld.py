69f16fa2c4b1d1e5dc573cfa5f0d51eff7c30260
Anthony LoGreco <aclogreco@gmail.com>
2016-08-23 21:15:27 -0400

    Create the pygameHelloWorld.py module.

---
+++ ch17/pygameHelloWorld.py
@@ -0,0 +1,12 @@
+# pygameHelloWorld.py
+# Hello, World! (pygame version)
+"""
+Hello, World! - a simple demonstation of the pygame library
+An example from Chapter 17 of
+'Invent Your Own Games With Python' by Al Sweigart
+
+A.C.LoGreco
+"""
+
+import pygame, sys
+from pygame.locals import *
