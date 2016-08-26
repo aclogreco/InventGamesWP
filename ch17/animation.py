0c5fde474bb70ad376f40968550105a51046205a
Anthony LoGreco <aclogreco@gmail.com>
2016-08-26 13:39:45 -0400

    Add the setup statements to the animation.py module.

--- ch17/animation.py
+++ ch17/animation.py
@@ -7,3 +7,36 @@ An example from Chapter 17 of
 
 A.C. LoGreco
 """
+
+imprt pygame, sys, time
+from pygame.locals import *
+
+# set up pygame
+pygame.init()
+
+# set up the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation')
+
+# set up direction vaiables
+DOWNLEFT = 1
+DOWNRIGHT = 3
+UPLEFT = 7
+UPRIGHT = 9
+
+MOVESPEED = 4
+
+# set up the colors
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# set up the block data structure
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+blocks = [b1, b2, b3]
+
