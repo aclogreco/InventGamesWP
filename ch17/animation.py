1732fe53dc228da64f3536ce2c76b420d8b100dc
Anthony LoGreco <aclogreco@gmail.com>
2016-08-26 13:27:39 -0400

    Create the animation.py module.

---
+++ ch17/animation.py
@@ -0,0 +1,9 @@
+# animation.py
+# Animation
+"""
+This is an example of animation using pygame.
+An example from Chapter 17 of
+'Invent Your Own Games With Python' by Al Sweigart
+
+A.C. LoGreco
+"""
