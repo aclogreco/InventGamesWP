8b0ef13b4e6b98b359fbd910435c73a878fc376f
Anthony LoGreco <aclogreco@gmail.com>
2016-09-01 19:52:06 -0400

    Create the collisionDetection.py module with heading comments.

---
+++ ch18/collisionDetection.py
@@ -0,0 +1,9 @@
+# collisionDetection.py
+# Collision Detection
+"""
+This is a demonstration of doing collison detection with pygame.
+An example from Chapter 18 of
+'Invent Your Own Games With Python' by Al Sweigart
+
+A.C. LoGreco
+"""
