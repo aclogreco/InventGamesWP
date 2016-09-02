5777bde31c3e23554f2411824cc7f77dd703841a
Anthony LoGreco <aclogreco@gmail.com>
2016-09-01 21:22:38 -0400

    Add the setup code to the collisionDetection.py module.

--- ch18/collisionDetection.py
+++ ch18/collisionDetection.py
@@ -39,3 +39,41 @@ def isPointInsideRect(x, y, rect):
         return True
     else:
         return False
+
+
+# set up pygame
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# set up the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+
+pygame.display.set_caption('Collision Detection')
+
+# set up direction variables
+DOWNLEFT = 1
+DOWNRIGHT = 3
+UPLEFT = 7
+UPRIGHT = 9
+
+MOVESPEED = 4
+
+# set up the colors
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+# set up the bouncer and food data structures
+foodCounter = 0
+NEWFOOD = 40
+FOODSIZE = 20
+bouncer = {'rect':pygame.Rect(300, 100, 50, 50), 'dir':UPLEFT}
+foods = []
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE),
+                             random.randint(0, WINDOWHEIGHT - FOODSIZE),
+                             FOODSIZE, FOODSIZE))
+
+
