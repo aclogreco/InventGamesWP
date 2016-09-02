2a8f357faa66b52045ba278e728ad96f4dbdc83a
Anthony LoGreco <aclogreco@gmail.com>
2016-09-01 20:14:43 -0400

    Add the functions doRectsOverlap() and isPointInsideRect() to the collisionDetection.py module.

--- ch18/collisionDetection.py
+++ ch18/collisionDetection.py
@@ -7,3 +7,35 @@ An example from Chapter 18 of
 
 A.C. LoGreco
 """
+
+import pygame, sys, random
+from pygame.locals import *
+
+
+def doRectsOverlap(rect1, rect2):
+    """
+    This function checks if two rectangles overlap.
+    Returns TRUE if they do and FALSE if they do not.
+    """
+    for a, b in [(rect1, rect2), (rect2, rect1)]:
+        # Check if a's corners are inside b
+        if ((isPointInsideRect(a.left, a.top, b)) or 
+            (isPointInsideRect(a.left, a.bottom, b)) or 
+            (isPointInsideRect(a.right, a.top, b)) or 
+            (isPointInsideRect(a.right, a.bottom, b))):
+            return True
+
+    return False
+
+def isPointInsideRect(x, y, rect):
+    """
+    This function determines if the point x, y is within the rectangle rect.
+    Returns TRUE if it is and FALSE if it is not.
+    """
+    if ((x > rect.left) and
+        (x < rect.right) and
+        (y > rect.top) and
+        (y < rect.bottom)):
+        return True
+    else:
+        return False
