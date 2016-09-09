4b4a0b3aaed2f1be6f1856e0981a4cbc4a87520f
Anthony LoGreco <aclogreco@gmail.com>
2016-09-08 21:49:29 -0400

    Add the game setup code to the pygameInput.py module.

--- ch18/pygameInput.py
+++ ch18/pygameInput.py
@@ -17,3 +17,39 @@ A.C. LoGreco
 
 import pygame, sys, random
 from pygame.locals import *
+
+# set up pygame
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# set up the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Input')
+
+# set up the colors
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+# set up the player and the food data structure
+foodCounter = 0
+NEWFOOD = 40
+FOODSIZE = 20
+player = pygame.Rect(300, 100, 50, 50)
+foods = []
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE),
+                             random.randint(0, WINDOWHEIGHT - FOODSIZE),
+                             FOODSIZE, FOODSIZE))
+
+# set up the movement variables
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False
+
+MOVESPEED = 6
+
+
