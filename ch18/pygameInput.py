9f8c54cfb77429c69b8a5be001e5205e83113e25
Anthony LoGreco <aclogreco@gmail.com>
2016-09-08 21:38:51 -0400

    Create the pygameInput.py module.

---
+++ ch18/pygameInput.py
@@ -0,0 +1,19 @@
+# pygameInput.py
+# Collision Detection
+#   with Keyboard and Mouse Input
+"""
+This is a demonstration of doing
+collison detection with pygame.
+This also demonstrates how to get user
+input drom the keyboard and the mouse
+using pygame.
+
+An example from Chapter 18 of
+'Invent Your Own Games With Python'
+  by Al Sweigart
+
+A.C. LoGreco
+"""
+
+import pygame, sys, random
+from pygame.locals import *
