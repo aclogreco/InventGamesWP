9c03e5f132f645fa88ed71a11a2da9fb58b4250f
Anthony LoGreco <aclogreco@gmail.com>
2016-12-17 00:08:08 -0500

    Add the main game loop to the pygameInput.py module.

--- ch18/pygameInput.py
+++ ch18/pygameInput.py
@@ -5,7 +5,7 @@
 This is a demonstration of doing
 collison detection with pygame.
 This also demonstrates how to get user
-input drom the keyboard and the mouse
+input from the keyboard and the mouse
 using pygame.
 
 An example from Chapter 18 of
@@ -53,3 +53,80 @@ moveDown = False
 MOVESPEED = 6
 
 
+# run the game loop
+while True:
+    # check for events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            # change the keyboard variables
+            if event.key == K_LEFT or event.key == ord('a'):
+                moveRight == False
+                moveLeft == True
+            if event.key == K_RIGHT or event.key == ord('d'):
+                moveRight == True
+                moveLeft == False
+            if event.key == K_UP or event.key == ord('w'):
+                moveDown == False
+                moveUp == True
+            if event.key == K_DOWN or event.key == ord('s'):
+                moveDown == True
+                moveUp == False
+        if event.type == KEYUP:
+            if event.key = K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            if event.key == K_LEFT or event.key == ord('a'):
+                moveLeft == False
+            if event.key == K_RIGHT or event.key == ord('d'):
+                moveRight == False
+            if event.key == K_UP or event.key == ord('w'):
+                moveUp == False
+            if event.key == K_DOWN or event.key == ord('s'):
+                moveDown == False
+            if event.key == ord('x'):
+                player.top = random.randint(0, WINDOWHEIGHT - player.height)
+                player.left = random.randint(0, WINDOWWIDTH - player.width)
+        if event.type == MOUSEBUTTONUP:
+            foods.append(pygame.Rect(event.pos[0], event.pos[1], FOODSIZE,
+                                     FOODSIZE))
+
+        foodCounter += 1
+        if foodCounter >= NEWFOOD:
+            # add new food
+            foodCounter = 0
+            foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE),
+                                     random.randint(0, WINDOWHEIGHT - FOODSIZE),
+                                     FOODSIZE,
+                                     FOODSIZE))
+
+        # draw the black background onto the surface
+        windowSurface.fill(BLACK)
+
+        # move the player
+        if moveDown and player.bottom < WINDOWHEIGHT:
+            player.top += MOVESPEED
+        if moveUp and player.top > 0:
+            player.top -= MOVESPEED
+        if moveLeft and player.left > 0:
+            player.left -= MOVESPEED
+        if moveRight and player.right < WINDOWWIDTH:
+            player.right += MOVESPEED
+
+        # draw the player onto the surface
+        pygame.draw.rect(windowSurface, WHITE, player)
+
+        # check if the player has intersected with any food squares
+        for food in foods[:]:
+            if player.colliderect(food):
+                foods.remove(food)
+
+        # draw the food
+        for i in range(len(foods)):
+            pygame.draw.rect(windowSurface, GREEN, foods[i])
+
+        # draw the window onto the screen
+        pygame.display.update()
+        mainClock.tick(40)
