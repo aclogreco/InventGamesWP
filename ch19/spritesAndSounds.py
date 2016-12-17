c114bc10b52cec7458d7615305c557ad98633d07
Anthony LoGreco <aclogreco@gmail.com>
2016-12-17 15:13:14 -0500

    Add the game setup code to the spritesAndSounds.py module.

--- ch19/spritesAndSounds.py
+++ ch19/spritesAndSounds.py
@@ -17,3 +17,49 @@ An example from Chapter 19 of
 
 A.C. LoGreco
 """
+
+import pygame, sys, time, random
+from pygame,locals import *
+
+# set up pygame
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# set up the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Sprites and Sound')
+
+# set up the colors
+BLACK = (0, 0, 0)
+
+# set up the block data structure
+# player
+player = pygame.Rect(300, 100, 40, 40)
+playerImage = pygame.image.load('.\sprites\player.png')
+playerStretchedImage = pygame.transform.scale(playerImage, (40, 40))
+# food
+foodImage = pygame.image.load('.\sprites\cherry.png')
+foods = []
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - 20),
+                             random.randint(0, WINDOWHEIGHT - 20), 20, 20))
+
+foodCounter = 0
+NEWFOOD = 40
+
+# set up keyboard variables
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False
+
+MOVESPEED = 6
+
+# set up music
+pickUpSound = pygame.mixer.Sound(file='.\sounds\pickup.wav')
+pygame.mixer.music.load('.\sounds\background.mid')
+pygame.mixer.music.play(-1, 0.0)
+musicPlaying = True
+
