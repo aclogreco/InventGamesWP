8b04744d9424e671ddb335748ac5a8192049e3a7
Anthony LoGreco <aclogreco@gmail.com>
2016-12-17 11:35:07 -0500

    Create the spritesAndSounds.py module and add the demos assets.

---
+++ ch19/spritesAndSounds.py
@@ -0,0 +1,19 @@
+# spritesAndSounds.py
+# Collision Detection
+#   with Keyboard and Mouse Input
+#   and with spites and sounds
+"""
+This is a demonstration of doing
+collison detection with pygame.
+This also demonstrates how to get user
+input from the keyboard and the mouse
+using pygame. The use of Sprites,
+sound, and music are demonstrated as
+well.
+
+An example from Chapter 19 of
+'Invent Your Own Games With Python'
+  by Al Sweigart
+
+A.C. LoGreco
+"""
