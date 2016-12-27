0c9649dbc60e53120636982dae834a5d4e89e286
Anthony LoGreco <aclogreco@gmail.com>
2016-12-27 00:00:31 -0500

    dodger.py: Add constants and helper functions.

--- ch20/dodger.py
+++ ch20/dodger.py
@@ -18,3 +18,52 @@ An example from Chapter 20 of
 
 A.C. LoGreco
 """
+
+
+import pygame, random, sys
+from pygame.locals import *
+
+
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOR = (0, 0, 0)
+FPS = 40
+BADDIEMINSIZE = 10
+BADDIEMAXSIZE = 40
+BADDIEMINSPEED = 1
+BADDIEMAXSPEED = 8
+ADDNEWBADDIERATE = 6
+PLAYERMOVERATE = 5
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()     # quit the game when escape key is pressed
+                return
+
+
+def playerHasHitBaddie(playerRect, baddies):
+    for b in baddies:
+        if playerRect.colliderect(b['rect']):
+            return True
+    return False
+
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
