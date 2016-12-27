d5fc3d586cf0e1c589f94a4e2075a1c57ed3ec4c
Anthony LoGreco <aclogreco@gmail.com>
2016-12-26 23:45:44 -0500

    Create the dodger.py module and add the game's assets.

---
+++ ch20/dodger.py
@@ -0,0 +1,20 @@
+# dodger.py
+# Dodger
+#   A simple graphical game where the
+#   player must dosge the bad guys.
+"""
+The Dodger game has the player control
+a small person (which we call the
+player’s character) who must dodge
+a whole bunch of baddies that fall
+from the top of the screen. The
+longer the player can keep dodging
+the baddies, the higher the score they
+will get.
+
+An example from Chapter 20 of
+'Invent Your Own Games With Python'
+  by Al Sweigart
+
+A.C. LoGreco
+"""
