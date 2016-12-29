5ef7a40f87990f862b2bacd1f4fe9f935ce4f073
Anthony LoGreco <aclogreco@gmail.com>
2016-12-28 20:37:56 -0500

    dodger.py: Finish the main game loop.

--- ch20/dodger.py
+++ ch20/dodger.py
@@ -226,4 +226,15 @@ while True:
 
         mainClock.tick(FPS)
 
-    
+    # Stop the game and show the "Game Over" screen.
+    pygame.mixer.music.stop()
+    gameOverSound.play()
+
+    drawText('GAME OVER', font, windowSurface,
+             (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+    drawText('Press a key to play again.', font, windowSurface,
+             (WINDOWWIDTH / 3) - 80, (WINDOWHEIGHT / 3) + 50)
+    pygame.display.update()
+    waitForPlayerToPressKey()
+
+    gameOverSound.stop()
